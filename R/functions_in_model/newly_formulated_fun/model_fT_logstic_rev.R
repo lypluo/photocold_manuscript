728ff1c510501b8e257ea18b732f249b5bbdd69e
lypluo <yluo@bgc-jena.mpg.de>
2022-06-23 15:29:20 +0200

    update code

---
+++ R/functions_in_model/newly_formulated_fun/model_fT_logstic_rev.R
@@ -0,0 +1,73 @@
+#################################################
+##considering the temperature modifier f_Ts(s stands for State of acclimation)
+##refer Mekela et al., 2004 and Mekela et al., 2008
+##also add a scaler to improve the GPP in the mid-season
+#################################################
+#
+f_Ts_logstic_rev<-function(df,
+     par=c("tau"=5,"X0"=-10,"b"=0.2,"Ts"=5,"k"=1),
+     plot=FALSE){
+
+  ##parameters:
+  #tau:represents the speed of response of the current acclimation status to
+  #changes in temperature (Tian et al., 2021)
+  #X0:lower temperature limit parameter(degreeC) above which f_Ts>0 and on the
+     #state of acclimation(Tian et al., 2021)
+  #b and Ts are the parameters for the sigmoid function of state of acclimation(S),
+  #b-->is curvature of the sigmoid function and 
+  #Ts is the inflection point of the sigmoid curve(the temperature at which
+  #f_Ts reach half of the maximum(0.5))-->Gea-Izquierdo et al.,2010
+
+  ## data frame df must contain columns:
+  ## 'tmin': daily minimum temperature
+  ## "doy"': day of the year
+  #initial setup:
+  f_stress<- rep(NA, nrow(df)) #f_stress:modifying factor by Temperature
+  # S<-rep(NA,nrow(df)) #S:state of acclimation
+  X<-rep(NA,nrow(df)) #X:the intermediate variable that used to calculate S
+
+
+  #
+  for (idx in seq(nrow(df))){
+    #---------
+    #1):X-->calcuating the intermediate values:
+    #---------
+    if(idx==1){
+      X[idx]=df$tmin[idx]
+    }
+    #
+    if(idx>1){
+      X[idx]=X[idx-1]+c(df$tmin[idx]-X[idx-1])/par["tau"]
+    }
+
+    #---------
+    #2):S-->state of acclimation
+    #---------
+    # S[idx]<-max(0, (X[idx] - par["X0"]))
+      S<-max(0, (X[idx] - par["X0"]))
+    #-----------
+    #3):f_stress:modifying factor for low temperature
+    #-----------
+    # f_stress[idx]<-min(S[idx]/par["Smax"],1)
+    f_stress_temp<-1/(1+exp((-1) *par["b"] * (S - par["Ts"])))
+    #---------
+    #(4):stress factor multiplied by a scalar to
+    ## allow for higher mid-season GPP after down-scaling early season GPP
+    ## set different scaler at different period
+    #between 4-01(from April air temperature starts >0) and 9-22(autumn equinox)
+    #:k1-->no T limitation
+    #other period:k0
+    #---------
+    if(df$doy[idx]>121 & df$doy[idx]<266){
+      f_stress[idx] <- f_stress_temp*par["k"]
+    }else
+    { f_stress[idx] <- f_stress_temp}
+  }
+
+  #plotting for evaluation:
+  if(plot){
+    plot(f_stress)
+  }
+  #
+  return(f_stress)
+}

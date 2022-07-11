d83d8c807a3735d027fbb15bcd7374efdf8b307b
lypluo <yluo@bgc-jena.mpg.de>
2022-07-11 19:13:05 +0200

    comaring the performance of different parameterization

--- R/functions_in_model/newly_formulated_fun/model_fT_rev.R
+++ R/functions_in_model/newly_formulated_fun/model_fT_rev.R
@@ -69,3 +69,62 @@ f_Ts_rev<-function(df,
   #
   return(f_stress)
 }
+
+
+#update: add additional parameter m in 2022-07:
+f_Ts_rev_2pars<-function(df,
+                   par=c("tau"=5,"X0"=-10,"Smax"=5,"k"=1,"m"=1),
+                   plot=FALSE){
+  ## data frame df must contain columns:
+  ## 'tmin': daily minimum temperature
+  ## "doy"': day of the year
+  #initial setup:
+  f_stress<- rep(NA, nrow(df)) #f_stress:modifying factor by Temperature
+  # S<-rep(NA,nrow(df)) #S:state of acclimation
+  X<-rep(NA,nrow(df)) #X:the intermediate variable that used to calculate S
+  
+  
+  #
+  for (idx in seq(nrow(df))){
+    #---------
+    #1):X-->calcuating the intermediate values:
+    #---------
+    if(idx==1){
+      X[idx]=df$tmin[idx]
+    }
+    #
+    if(idx>1){
+      X[idx]=X[idx-1]+c(df$tmin[idx]-X[idx-1])/par["tau"]
+    }
+    
+    #---------
+    #2):S-->state of acclimation
+    #---------
+    # S[idx]<-max(0, (X[idx] - par["X0"]))
+    S<-max(0, (X[idx] - par["X0"]))
+    #-----------
+    #3):f_stress:modifying factor for low temperature
+    #-----------
+    # f_stress[idx]<-min(S[idx]/par["Smax"],1)
+    f_stress_temp<-min(S/par["Smax"],1)
+    #---------
+    #(4):stress factor multiplied by a scalar to
+    ## allow for higher mid-season GPP after down-scaling early season GPP
+    ## set different scaler at different period
+    #between 4-01(from April air temperature starts >0) and 9-22(autumn equinox)
+    #:k1-->no T limitation
+    #other period:k0
+    #---------
+    if(df$doy[idx]>121 & df$doy[idx]<266){
+      f_stress[idx] <- f_stress_temp*par["k"]
+    }else
+    { f_stress[idx] <- f_stress_temp*par["m"]}
+  }
+  
+  #plotting for evaluation:
+  if(plot){
+    plot(f_stress)
+  }
+  #
+  return(f_stress)
+}
---
+++ test/comp_diff_parameterization_performance/readme.txt
@@ -0,0 +1,3 @@
+Aim: in this test, we mainly would like to compare the performance with comparing two different parameterization: 
+1) one is the method from Beni's method (using two logistic functions)
+2) another is adopting the method from Mekela et al., 2004-2008
